d05c82d91b50a5c209a3d9b5a23f8e2fab426ca2
carnage67694325 <kingabdoeid2014@gmail.com>
2025-06-04 12:24:40 +0300

    build report problem cubit

---
+++ lib/features/report_problem/view/view_model/report_problem/report_problem_cubit.dart
@@ -0,0 +1,40 @@
+import 'package:bloc/bloc.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:doct_plant/constants/endpoints.dart';
+import 'package:doct_plant/core/Preferences/prefs_handler.dart';
+import 'package:doct_plant/core/errors/failure.dart';
+import 'package:doct_plant/core/utils/api_service.dart';
+import 'package:meta/meta.dart';
+
+part 'report_problem_state.dart';
+
+class ReportProblemCubit extends Cubit<ReportProblemState> {
+  final ApiService apiService;
+  ReportProblemCubit(this.apiService) : super(ReportProblemInitial());
+  Future<Either<Failure, void>> login(String desc) async {
+    emit(ReportProblemLoading());
+
+    try {
+      String? userToken = await PrefasHandelr.getAuthToken();
+      await apiService.postReportProblem(
+          endpoint: Endpoints.kReportProblem,
+          body: {
+            "descripcion": desc,
+          },
+          userToken: userToken!);
+
+      emit(ReportProblemSuccess());
+
+      return right(null);
+    } on DioException catch (dioError) {
+      final failure = ServerFailure.fromDioException(dioError);
+      emit(ReportProblemFaiulre(errMessage: failure.errMessage));
+      return left(failure);
+    } catch (e) {
+      final failure = ServerFailure(e.toString());
+      emit(ReportProblemFaiulre(errMessage: failure.errMessage));
+      return left(failure);
+    }
+  }
+}
---
+++ lib/features/report_problem/view/view_model/report_problem/report_problem_state.dart
@@ -0,0 +1,16 @@
+part of 'report_problem_cubit.dart';
+
+@immutable
+sealed class ReportProblemState {}
+
+final class ReportProblemInitial extends ReportProblemState {}
+
+final class ReportProblemLoading extends ReportProblemState {}
+
+final class ReportProblemSuccess extends ReportProblemState {}
+
+final class ReportProblemFaiulre extends ReportProblemState {
+  final String errMessage;
+
+  ReportProblemFaiulre(this.errMessage);
+}
